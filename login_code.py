774c565640e10d645ac0a6711095838e8a1f5f5e
R1bat <143386988+R1bat@users.noreply.github.com>
2023-11-02 14:14:49 +0700

    Update login_code.py
    Update kode dengan databse generator, signup akan menmabah file database json

--- login_code.py
+++ login_code.py
@@ -4,64 +4,86 @@ Created on Wed Oct 25 16:55:29 2023
 
 @author: Arribaat
 """
-
+import os
 import getpass
 import json
 
+#var penyimpanan data user sementara
+stripvar = "---------------------"
+
 def greeting():
-    print("-------------------------".center(110))
-    print("Selamat datang di Wallabi ".center(110))
-    print("Konsultan keuangan digital".center(110))
-    print("-------------------------".center(110))
+    os.system('cls')
+    print(stripvar*4)
+    print("Selamat datang di Wallabi ".center(90))
+    print("Konsultan keuangan digital".center(90))
+    print(stripvar*4)
     print("")
-    
-greeting()
 
-def sign_up():
-    client = {"username":"",
-              "password":""}
-    username = input("Masukkan username: ")
-    password = getpass.getpass("Masukkan password: ")
-    konfirmasi = getpass.getpass("Konfirmasi password: ")
-    if konfirmasi == password:
-        print("Sandi cocok")
-        client["username"] = username
-        client["password"] = password
-        
-        with open("data_user.json",'w') as file:
-            json.dump(client,file)
-            
-    else:
-        print("Sandi tidak cocok")
-
-def login():
-    username = input("Masukkan username: ")
-    password = getpass.getpass("Masukkan password: ")
-    
-    with open("data_user.json") as datauser:
-        data_user = json.load(datauser)
-    #if username dan password ono neng db:
-        #print("login berhasil")
-    #else:
-        #print("login gagal")
-    if username == data_user["username"] :
-        if password == data_user["password"] :
-            print("Login Berhasil!")
-            
-        else :
-            print("Login Gagal, mohon ingat password anda")
-    else :
-        print("Username Tidak Ditemukan","\n mohon sign up terlebih dahulu")
-        
-while True:
+def init():
     print("1. Login")
     print("2. Sign Up")
     choice = int(input("Pilih menu: "))
     if choice == 1:
+        greeting()
         login()
-        break
+        
     elif choice == 2:
+        greeting()
         sign_up()
-        break
+        
     else:
-        print("Invalid choice. Silahkan masukkan 1 atau 2")
+        print("Invalid choice.\nSilahkan masukkan 1 atau 2")
+        
+def sign_up():
+    client = {"password":""}
+    username = input("Masukkan username: ")
+    password = getpass.getpass("Masukkan password: ")
+    konfirmasi = getpass.getpass("Konfirmasi password: ")
+    if konfirmasi == password:
+        greeting()
+        print("Sandi cocok")
+        client["password"] = password
+        
+        with open(username + ".json",'x') as file:
+            json.dump(client,file)
+        
+        init()
+            
+    else:
+        greeting()
+        print("Sandi tidak cocok")
+        init()
+
+def login():
+    
+    username = input("Masukkan username: ")
+    
+    try :
+        with open(username + ".json") as datauser:
+            data_user = json.load(datauser)
+        file_exist = True
+    except:
+        file_exist =  False
+    
+    if file_exist == True :
+        password = getpass.getpass("Masukkan password: ")
+        if password == data_user["password"] :
+            print("Login Berhasil!")
+            
+        else :
+            greeting()
+            print("Login Gagal, mohon ingat password anda")
+            print("")
+            init()
+            
+    elif file_exist == False:
+        greeting()
+        print("Username Tidak Ditemukan")
+        print("")
+        init()
+
+greeting()        
+init()
+
+#write data di akhir program ketika program berhenti
+#selama program masih berjalan, maka data akan disimpan variabel di dalam terminal
